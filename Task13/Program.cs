2b97f296ecd273873cfd1eb596fd14bdbde56562
AlexandrSawrasov <caiiiuk@mail.ru>
2022-10-20 00:20:51 +0300

    add Task 13 2

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -6,14 +6,33 @@
 
 // 32679 -> 6
 
+// Console.Write("Введите число: ");
+// string? number = Console.ReadLine();
+// if(int.TryParse(number, out int num))
+// {
+//     string num1 = Convert.ToString(Math.Abs(num));
+//     if (Math.Abs(num) > 99)
+//     {
+//         Console.Write($"Третья цифра введенного числа: {number[2]}");
+//     }
+//     else Console.Write("Третьей цифры нет");
+// }
+// else Console.Write("Введеное выражение не является числом");
+
 Console.Write("Введите число: ");
 string? number = Console.ReadLine();
-if(int.TryParse(number, out int num))
+if (int.TryParse(number, out int num))
 {
-    string num1 = Convert.ToString(Math.Abs(num));
+    int depth = number.Length;
+    if (num < 0) depth--;
     if (Math.Abs(num) > 99)
     {
-        Console.Write($"Третья цифра введенного числа: {num1[2]}");
+        for (int i = depth; i != 3; i--)
+        {
+            num /= 10;
+        }
+        num %= 10;
+        Console.Write($"Третья цифра введенного числа: {Math.Abs(num)}");
     }
     else Console.Write("Третьей цифры нет");
 }
